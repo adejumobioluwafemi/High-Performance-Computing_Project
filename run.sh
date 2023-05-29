72496b079136a2267dcad417e17bbf61abbdcabb
YodaX369 <koffivigbagbe@gmail.com>
2023-05-29 16:40:49 -0700

    Add run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Run the "test" executable and record its execution time
@@ -13,3 +14,20 @@ py_time=$(echo "$(date +%s.%N) - $start_time" | bc)
 # Output the execution times
 echo "Execution time for test: $test_time seconds"
 echo "Execution time for python script: $py_time seconds"
+
+:'
+#!/bin/bash
+
+# Run the test executable and redirect its output to test.txt
+time ./test > test.txt
+
+# Run the pytorch.py script and redirect its output to pytorch.txt
+time python pytorch.py > pytorch.txt
+
+# Print the execution time for test and pytorch.py to the screen
+echo "Execution time for test:"
+time ./test >/dev/null 2>&1
+
+echo "Execution time for pytorch.py:"
+time python pytorch.py >/dev/null 2>&1
+'
